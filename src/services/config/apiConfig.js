be4f3102176de0bf43636c1bce9bc1c285999ff2
drsky <contact@drsky.ovh>
2023-09-11 13:18:30 +0200

    move: transfer mock data to public folder and remove src/data/

--- src/services/config/apiConfig.js
+++ src/services/config/apiConfig.js
@@ -3,22 +3,22 @@ import { formatActivityData, formatAverageSessionsData, formatPerformanceData, f
 export const API_CONFIG = {
   user: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/`,
-    mocked: (userId) => `/src/data/${userId}/user.json`,
+    mocked: (userId) => `/P12/data/${userId}/user.json`,
     formatDataFunction: formatUserInfosData
   },
   activity: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/activity`,
-    mocked: (userId) => `/src/data/${userId}/activity.json`,
+    mocked: (userId) => `/P12/data/${userId}/activity.json`,
     formatDataFunction: formatActivityData
   },
   averageSessions: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/average-sessions`,
-    mocked: (userId) => `/src/data/${userId}/average-sessions.json`,
+    mocked: (userId) => `/P12/data/${userId}/average-sessions.json`,
     formatDataFunction: formatAverageSessionsData
   },
   performance: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/performance`,
-    mocked: (userId) => `/src/data/${userId}/performance.json`,
+    mocked: (userId) => `/P12/data/${userId}/performance.json`,
     formatDataFunction: formatPerformanceData
   }
 };
