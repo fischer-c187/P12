bcaa71b9be6bdb5822e6ccf73cce0ae55657ba73
drsky <contact@drsky.ovh>
2023-09-12 09:40:38 +0200

    Add constant for menu text

---
+++ src/constants/menuTexts.js
@@ -0,0 +1,20 @@
+const MENU_TEXT = [
+  {
+    text: 'Accueil',
+    link: '/'
+  },
+  {
+    text: 'Profil',
+    link: '#'
+  },
+  {
+    text: 'Réglage',
+    link: '#'
+  },
+  {
+    text: 'Communauté',
+    link: '#'
+  }
+];
+
+export default MENU_TEXT;
