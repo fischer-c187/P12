9f4fcb890a4aee3b24b3303e655d9fd2671f9106
drsky <contact@drsky.ovh>
2023-09-13 10:47:54 +0200

    Add JSDoc comments to sidebar and sidebarText

--- src/constants/sidebarText.jsx
+++ src/constants/sidebarText.jsx
@@ -3,6 +3,16 @@ import Swim from '../assets/swim.svg';
 import Bike from '../assets/bike.svg';
 import Body from '../assets/bodybuilding.svg';
 
+/**
+ * @typedef {Object} NavItem
+ * @property {JSX.Element} text - Display element for the navigation link.
+ * @property {string} link - URL or path the link points to.
+ */
+
+/**
+ * Array of navigation items for the sidebar, including icons and link destinations.
+ * @type {NavItem[]}
+ */
 const SIDEBAR_NAV_TEXT = [
   {
     text: <img src={Yoga} />,
--- src/layouts/sidebar/sidebar.jsx
+++ src/layouts/sidebar/sidebar.jsx
@@ -1,10 +1,22 @@
 import NavBar from '../../components/navBar/navBar';
 import './sidebar.scss';
 
+/**
+ * @typedef {Object} NavItem
+ * @property {JSX.Element | string} text - Display content (text or JSX) for the navigation link.
+ * @property {string} link - URL or path the link points to.
+ */
+
+/**
+ * Component to render the sidebar. Contains a navigation bar to switch between different sports and a copyright notice.
+ * 
+ * @param {NavItem[]} navItems - Array of navigation items for the sidebar.
+ * @returns {JSX.Element} Rendered Sidebar component.
+ */
 function Sidebar({ navItems }) {
   return (
     <aside className='sidebar'>
-      <NavBar navItems={navItems} className='sidebar-nav'/>
+      <NavBar navItems={navItems} className='sidebar-nav' />
       <p className='sidebar__copyright'>Copiryght, SportSee 2020</p>
     </aside>
   );
