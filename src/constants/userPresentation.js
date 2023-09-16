1601c33424c04bb5c6c1596056e2c2387a79595a
drsky <contact@drsky.ovh>
2023-09-17 00:24:41 +0200

    Add UserPresentation component for user greetings.

---
+++ src/constants/userPresentation.js
@@ -0,0 +1,11 @@
+/**
+ * Constants for welcome messages displayed to users.
+ * @namespace WELCOME_MESSAGE
+ * 
+ * @property {string} INCENTIVE - A congratulatory message for the user who has surpassed their goals.
+ */
+const WELCOME_MESSAGE = {
+  INCENTIVE: 'Félicitation ! Vous avez explosé vos objectifs hier 👏'
+};
+
+export default WELCOME_MESSAGE;
