1d75cba8445fcbb76070edd770feb171e56db52e
drsky <contact@drsky.ovh>
2024-03-01 12:24:27 +0100

    fix: Add BASE_URL env variable for deploying projects on different platforms

--- src/services/config/apiConfig.js
+++ src/services/config/apiConfig.js
@@ -1,29 +1,50 @@
-import { formatActivityData, formatAverageSessionsData, formatPerformanceData, formatUserInfosData } from '../data/format';
+import {
+  formatActivityData,
+  formatAverageSessionsData,
+  formatPerformanceData,
+  formatUserInfosData,
+} from "../data/format";
+
 /**
- * Describes the configuration for fetching data. 
+ * Cleans the base URL by removing trailing slashes.
+ * @param {string} baseUrl - The base URL.
+ * @returns {string} The cleaned base URL.
+ */
+const cleanBaseUrl = (baseUrl) => {
+  return baseUrl.replace(/\/$/, "");
+};
+
+/**
+ * The base URL for the API.
+ */
+const baseUrl = cleanBaseUrl(import.meta.env.VITE_BASE_URL);
+
+/**
+ * Describes the configuration for fetching data.
  * Provides paths for either mocked or backend data sources.
- * The `formatDataFunction` callback is utilized to transform 
+ * The `formatDataFunction` callback is utilized to transform
  * raw data into a display-ready format.
  */
 export const API_CONFIG = {
   user: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/`,
-    mocked: (userId) => `/P12/data/${userId}/user.json`,
-    formatDataFunction: formatUserInfosData
+    mocked: (userId) => `${baseUrl}/data/${userId}/user.json`,
+    formatDataFunction: formatUserInfosData,
   },
   activity: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/activity`,
-    mocked: (userId) => `/P12/data/${userId}/activity.json`,
-    formatDataFunction: formatActivityData
+    mocked: (userId) => `${baseUrl}/data/${userId}/activity.json`,
+    formatDataFunction: formatActivityData,
   },
   averageSessions: {
-    endpoint: (userId) => `http://localhost:3000/user/${userId}/average-sessions`,
-    mocked: (userId) => `/P12/data/${userId}/average-sessions.json`,
-    formatDataFunction: formatAverageSessionsData
+    endpoint: (userId) =>
+      `http://localhost:3000/user/${userId}/average-sessions`,
+    mocked: (userId) => `${baseUrl}/data/${userId}/average-sessions.json`,
+    formatDataFunction: formatAverageSessionsData,
   },
   performance: {
     endpoint: (userId) => `http://localhost:3000/user/${userId}/performance`,
-    mocked: (userId) => `/P12/data/${userId}/performance.json`,
-    formatDataFunction: formatPerformanceData
-  }
+    mocked: (userId) => `${baseUrl}/data/${userId}/performance.json`,
+    formatDataFunction: formatPerformanceData,
+  },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process"; // Import the 'process' module
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/P12/'
+// VITE_BASE_URL is the base URL for the API.
+// It is used to fetch data from either mocked or backend data sources.
+//
+// the different base is needed because the app is deployed on different environments
+// '/p12' for github pages
+// '/' for netlify or vercel
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+  };
 });
